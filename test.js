d0d804721b443b3d381fa866e980bddfd9750d7b
Eduard Castellano <educastellano08@gmail.com>
2025-12-09 17:13:17 +0100

    Add support to get entries

--- test.js
+++ test.js
@@ -10,3 +10,36 @@ test('load .jpg', (t) => {
 
   t.comment(data)
 })
+
+test('entry has raw data', (t) => {
+  const image = require('./test/fixtures/grapefruit.jpg', {
+    with: { type: 'binary' }
+  })
+
+  const data = new exif.Data(image)
+  const entry = data.entry(exif.constants.tags.ORIENTATION)
+
+  t.is(entry.tag, 274)
+  t.is(entry.format, 3)
+  t.is(entry.components, 1)
+  t.is(entry.data.length, 2)
+  t.is(entry.data[0], 0)
+  t.is(entry.data[1], 1)
+  t.is(entry.size, 2)
+})
+
+test('print all entries', (t) => {
+  const image = require('./test/fixtures/grapefruit.jpg', {
+    with: { type: 'binary' }
+  })
+
+  const data = new exif.Data(image)
+
+  for (let [key, value] of Object.entries(exif.constants.tags)) {
+    const entry = data.entry(value)
+    if (entry) {
+      t.comment(`${key}: ${entry.value()}`)
+    }
+  }
+})
+
